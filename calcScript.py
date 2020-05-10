a926c343ce0544f31a677c08e376c639dd0e1a2a
ItayNetz <65127493+ItayNetz@users.noreply.github.com>
2020-05-10 21:10:22 +0300

    Update calcScript.py

--- calcScript.py
+++ calcScript.py
@@ -7,24 +7,27 @@ while True:
    print("Enter 'quit' to end the program")
    input_ = input("/")
 
-   if user_input == "quit":
+   if input_ == "quit":
       break
    elif input_ == "add":
       num1 = float(input("Enter a number: "))
       num2 = float(input("Enter another number: "))
       result = str(num1 + num2)
-  print("The answer is " + result
+      print("The answer is " + result)
    elif input_ == "subtract":
       num1 = float(input("Enter a number: "))
       num2 = float(input("Enter another number: "))
       result = str(num1 - num2)
+      print("The answer is " + result)
    elif input_ == "multiply":
       num1 = float(input("Enter a number: "))
       num2 = float(input("Enter another number: "))
       result = str(num1 * num2)
+      print("The answer is " + result)
    elif input_ == "divide":
       num1 = float(input("Enter a number: "))
       num2 = float(input("Enter another number: "))
       result = str(num1 / num2)
+      print("The answer is " + result)
    else:
       print("Unknown input")
